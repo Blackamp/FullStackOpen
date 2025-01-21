bdead3ffe4a8e42f932a88a4dac82941ff7ba009
Jose C. <blackamp8@gmail.com>
2025-01-21 19:31:45 +0100

    E-4.4 & E-4.5

--- part4/blogger/backend_blogger/test/dummy.test.js
+++ part4/blogger/backend_blogger/test/dummy.test.js
@@ -2,9 +2,12 @@ const { test, describe } = require('node:test')
 const assert = require('node:assert')
 const listHelper = require('../utils/list_helper')
 
-test('dummy returns one', () => {
-  const blogs = []
+// Conjunto de test fx dummy
+describe('dummy', () => {
+    test('dummy returns one', () => {
+    const blogs = []
 
-  const result = listHelper.dummy(blogs)
-  assert.strictEqual(result, 1)
+    const result = listHelper.dummy(blogs)
+    assert.strictEqual(result, 1)
+    })
 })
--- part4/blogger/backend_blogger/utils/list_helper.js
+++ part4/blogger/backend_blogger/utils/list_helper.js
@@ -1,7 +1,39 @@
+const blog = require("../models/blog")
+const logger = require("./logger")
+
+
 const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  const sumWithInitial = blogs.reduce((accumulator, currentValue, indice) => accumulator + blogs[indice].likes,0,)
+  //logger.info(sumWithInitial);
+  return sumWithInitial 
+}
+
+const favoriteBlog = (blogs) => {
+
+  if(blogs.length === 0)
+    return 0
+
+  const mostLikedBlog = blogs.reduce((mostLiked, currentBlog) => {
+    return (currentBlog.likes > mostLiked.likes) ? currentBlog : mostLiked;
+  });
+  //logger.info(mostLikedBlog);
+  const returnBlog = {
+    title: mostLikedBlog.title,
+    author: mostLikedBlog.author,
+    likes: mostLikedBlog.likes
+  }
+  //logger.info(mostLikedBlog);
+
+  return returnBlog
+
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes,
+  favoriteBlog,
 }
